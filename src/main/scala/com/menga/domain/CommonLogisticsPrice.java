35d441a5c3d51396210fca417df127abdceb4525
程孟威 <295567172@qq.com>
2019-09-20 19:03:59 +0800

    add excel

---
+++ src/main/scala/com/menga/domain/CommonLogisticsPrice.java
@@ -0,0 +1,137 @@
+package com.menga.domain;
+
+/**
+ * Created by Marvel on 2019/9/20.
+ */
+public class CommonLogisticsPrice {
+
+    private String sourceCodeProvince;
+    private String sourceCodeCity;
+    private String sourceCodeZone;
+    private String destinationCodeProvince;
+    private String destinationCodeCity;
+    private String destinationCodeZone;
+    private String price1st;
+    private String price2nd;
+    private String price3nd;
+    private String price4th;
+    private String price5th;
+    private String price6th;
+    private String price7th;
+    private String price8th;
+
+    public CommonLogisticsPrice() {
+    }
+
+    public String getSourceCodeProvince() {
+        return sourceCodeProvince;
+    }
+
+    public void setSourceCodeProvince(String sourceCodeProvince) {
+        this.sourceCodeProvince = sourceCodeProvince;
+    }
+
+    public String getSourceCodeCity() {
+        return sourceCodeCity;
+    }
+
+    public void setSourceCodeCity(String sourceCodeCity) {
+        this.sourceCodeCity = sourceCodeCity;
+    }
+
+    public String getSourceCodeZone() {
+        return sourceCodeZone;
+    }
+
+    public void setSourceCodeZone(String sourceCodeZone) {
+        this.sourceCodeZone = sourceCodeZone;
+    }
+
+    public String getDestinationCodeProvince() {
+        return destinationCodeProvince;
+    }
+
+    public void setDestinationCodeProvince(String destinationCodeProvince) {
+        this.destinationCodeProvince = destinationCodeProvince;
+    }
+
+    public String getDestinationCodeCity() {
+        return destinationCodeCity;
+    }
+
+    public void setDestinationCodeCity(String destinationCodeCity) {
+        this.destinationCodeCity = destinationCodeCity;
+    }
+
+    public String getDestinationCodeZone() {
+        return destinationCodeZone;
+    }
+
+    public void setDestinationCodeZone(String destinationCodeZone) {
+        this.destinationCodeZone = destinationCodeZone;
+    }
+
+    public String getPrice1st() {
+        return price1st;
+    }
+
+    public void setPrice1st(String price1st) {
+        this.price1st = price1st;
+    }
+
+    public String getPrice2nd() {
+        return price2nd;
+    }
+
+    public void setPrice2nd(String price2nd) {
+        this.price2nd = price2nd;
+    }
+
+    public String getPrice3nd() {
+        return price3nd;
+    }
+
+    public void setPrice3nd(String price3nd) {
+        this.price3nd = price3nd;
+    }
+
+    public String getPrice4th() {
+        return price4th;
+    }
+
+    public void setPrice4th(String price4th) {
+        this.price4th = price4th;
+    }
+
+    public String getPrice5th() {
+        return price5th;
+    }
+
+    public void setPrice5th(String price5th) {
+        this.price5th = price5th;
+    }
+
+    public String getPrice6th() {
+        return price6th;
+    }
+
+    public void setPrice6th(String price6th) {
+        this.price6th = price6th;
+    }
+
+    public String getPrice7th() {
+        return price7th;
+    }
+
+    public void setPrice7th(String price7th) {
+        this.price7th = price7th;
+    }
+
+    public String getPrice8th() {
+        return price8th;
+    }
+
+    public void setPrice8th(String price8th) {
+        this.price8th = price8th;
+    }
+}
--- src/main/scala/com/menga/mysql/POIDemo.scala
+++ src/main/scala/com/menga/mysql/POIDemo.scala
@@ -1,8 +1,95 @@
 package com.menga.mysql
 
+import java.io.File
+import java.io.FileInputStream
+
+import com.menga.domain.CommonLogisticsPrice
+import org.apache.poi.xssf.usermodel.{XSSFCell, XSSFRow, XSSFSheet, XSSFWorkbook}
+
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Created by Marvel on 2019/9/20.
   */
 class POIDemo {
 
+}
+
+object POIDemo {
+
+  def readExcel(): Unit = {
+    //打开需要读取的文件
+    val inputStream: FileInputStream = new FileInputStream(new File("第一批路线.xlsx"))
+    //读取工作簿
+    val wordBook: XSSFWorkbook = new XSSFWorkbook(inputStream)
+    //读取工作表,从0开始
+    val sheet: XSSFSheet = wordBook.getSheetAt(1)
+    //读取第三行
+    val row: XSSFRow = sheet.getRow(1)
+    //读取单元格
+    val cell: XSSFCell = row.getCell(1)
+    //获取单元格对象
+    val value: String = cell.getStringCellValue
+    System.out.println(value)
+    //关闭输入流
+    inputStream.close()
+    //关闭工作簿
+    wordBook.close()
+  }
+
+  def readLogisticsExcel(): List[CommonLogisticsPrice] = {
+    val filePath = "第一批路线.xlsx"
+    //打开需要读取的文件
+    val inputStream: FileInputStream = new FileInputStream(new File(filePath))
+    //读取工作簿
+    val wordBook: XSSFWorkbook = new XSSFWorkbook(inputStream)
+
+    val prices = dealLogisticsBook(wordBook)
+
+    //关闭输入流
+    inputStream.close()
+    //关闭工作簿
+    wordBook.close()
+
+    prices
+  }
+
+  def dealLogisticsBook(wordBook: XSSFWorkbook): List[CommonLogisticsPrice] = {
+    val sheetNums = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    sheetNums.flatMap(it => dealLogisticsSheet(wordBook.getSheetAt(it)))
+  }
+
+  def dealLogisticsSheet(sheet: XSSFSheet): List[CommonLogisticsPrice] = {
+    val rows = sheet.rowIterator()
+    val list: ArrayBuffer[CommonLogisticsPrice] = new ArrayBuffer[CommonLogisticsPrice]
+    while (rows.hasNext) {
+      val row = rows.next()
+      if (row.getRowNum != 0) {
+        val price = new CommonLogisticsPrice
+        price.setSourceCodeProvince(if (row.getCell(0) != null) row.getCell(0).getStringCellValue else "")
+        price.setSourceCodeCity(if (row.getCell(1) != null) row.getCell(1).getStringCellValue else "")
+        price.setSourceCodeZone(if (row.getCell(2) != null) row.getCell(2).getStringCellValue else "")
+        price.setDestinationCodeProvince(if (row.getCell(3) != null) row.getCell(3).getStringCellValue else "")
+        price.setDestinationCodeCity(if (row.getCell(4) != null) row.getCell(4).getStringCellValue else "")
+        price.setDestinationCodeZone(if (row.getCell(5) != null) row.getCell(5).getStringCellValue else "")
+        price.setPrice1st(if (row.getCell(6) != null) row.getCell(6).getStringCellValue else "")
+        price.setPrice2nd(if (row.getCell(7) != null) row.getCell(7).getStringCellValue else "")
+        price.setPrice3nd(if (row.getCell(8) != null) row.getCell(8).getStringCellValue else "")
+        price.setPrice4th(if (row.getCell(9) != null) row.getCell(9).getStringCellValue else "")
+        price.setPrice5th(if (row.getCell(10) != null) row.getCell(10).getStringCellValue else "")
+        price.setPrice6th(if (row.getCell(11) != null) row.getCell(11).getStringCellValue else "")
+        price.setPrice7th(if (row.getCell(12) != null) row.getCell(12).getStringCellValue else "")
+        price.setPrice8th(if (row.getCell(13) != null) row.getCell(13).getStringCellValue else "")
+        list += price
+      }
+    }
+    list.toList
+  }
+
+  def main(args: Array[String]): Unit = {
+//    readExcel()
+    val prices = readLogisticsExcel()
+    prices
+  }
 }
